952df44246ff2b299ac3463a47c244112b705ebb
Muhammad Tayyab <tj.tayyab1@outlook.com>
2024-06-14 00:54:48 +0500

    companies screen done

--- angular/src/app/companies/companies.module.ts
+++ angular/src/app/companies/companies.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CompaniesRoutingModule } from './companies-routing.module';
+import { TableModule } from 'primeng/table';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    TableModule,
     CompaniesRoutingModule
   ]
 })
--- angular/src/app/companies/components/companies/companies.component.ts
+++ angular/src/app/companies/components/companies/companies.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { DropdownModule } from "primeng/dropdown"; 
+import { CalendarModule } from 'primeng/calendar';
+import { ImageModule } from 'primeng/image';
+import { FileUploadModule } from 'primeng/fileupload';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-companies',
   standalone: true,
-  imports: [],
+  imports: [TableModule,AutoCompleteModule, FormsModule,DropdownModule,CalendarModule,ImageModule,FileUploadModule ],
   templateUrl: './companies.component.html',
   styleUrl: './companies.component.scss'
 })
 export class CompaniesComponent {
+  filteredCountries: any[];
+  companies: any[] = [ 
+    { company: "Riyadh",ticker:"IBL",ye:"DEC",est:"18-2-2024",order:"4501",alternate:"",st:"6565",eng:"Riyadd" }, 
+    { company: "Riyadh",ticker:"IBL",ye:"DEC",est:"18-2-2024",order:"4501",alternate:"",st:"6565",eng:"Riyadd" }, 
+    { company: "Riyadh",ticker:"IBL",ye:"DEC",est:"18-2-2024",order:"4501",alternate:"",st:"6565",eng:"Riyadd" }, 
+    { company: "Riyadh",ticker:"IBL",ye:"DEC",est:"18-2-2024",order:"4501",alternate:"",st:"6565",eng:"Riyadd" }, 
+  ];
+  markets = [ 
+    { name: "TASI" }, 
+    { name: "ReactJS" }, 
+    { name: "Angular" }, 
+    { name: "Bootstrap" }, 
+    { name: "PrimeNG" }, 
+  ]; 
 
+  ngOnInit() { 
+    this.filteredCountries = [
+      {name: "RIBL",code:'rible'},
+      {name: "Suadia Arabia",code:'KSA'},
+      {name: "Dubai",code:'UAE'},
+      {name: "IRAN",code:'IR'},
+    ]
+  }
+  
 }
