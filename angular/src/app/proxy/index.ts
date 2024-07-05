47b218534068dcb487a782fc71f8c60f7497d2a9
Muhammad Tayyab <tj.tayyab1@outlook.com>
2024-07-06 01:28:37 +0500

    CompaniesFieldsMappingDone

--- angular/src/app/proxy/index.ts
+++ angular/src/app/proxy/index.ts
@@ -1,4 +1,5 @@
 import * as Books from './books';
 import * as Commons from './commons';
+import * as Companies from './companies';
 import * as GbFacts from './gb-facts';
-export { Books, Commons, GbFacts };
+export { Books, Commons, Companies, GbFacts };
---
+++ aspnet-core/src/GB.Opera.Application.Contracts/Companies/ICompanyAppService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace Companies
+{
+    public interface ICompanyAppService : IApplicationService
+    {
+        Task<List<CompanyDto>> GetCompanies(int sectorID, int stockMarketID);
+
+    }
+}
