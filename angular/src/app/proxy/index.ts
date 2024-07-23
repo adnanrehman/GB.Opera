4efa8ff5d09229f5d40cfac2dfefebdfd4af97df
UMER <UMER@DESKTOP-2AD1NGM>
2024-07-23 22:38:04 +0500

    ff

--- angular/src/app/proxy/index.ts
+++ angular/src/app/proxy/index.ts
@@ -3,4 +3,4 @@ import * as Books from './books';
 import * as Commons from './commons';
 import * as Companies from './companies';
 import * as GbFacts from './gb-facts';
-export { AccountsClassifications, Books, Commons, Companies, CompanyManagements, GbFacts };
+export { AccountsClassifications, Books, Commons, Companies, GbFacts };
