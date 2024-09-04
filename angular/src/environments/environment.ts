60f4fa9701c53aa068e1fecfdeb36e2d5eca61c4
Muhammad Tayyab <tj.tayyab1@outlook.com>
2024-09-04 09:34:51 +0500

    DeployementSettingsDone

--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -1,6 +1,7 @@
 import { Environment } from '@abp/ng.core';
 
-const baseUrl = 'http://localhost:4200';
+const baseUrl = 'https://opera.gulfbase.com';
+// const baseUrl = 'http://localhost:4200';
 
 export const environment = {
   production: false,
@@ -10,7 +11,8 @@ export const environment = {
     logoUrl: '',
   },
   oAuthConfig: {
-    issuer: 'https://localhost:44349/',
+    issuer: 'https://operaapi.gulfbase.com/',
+    // issuer: 'https://localhost:44349/',
     redirectUri: baseUrl,
     clientId: 'Opera_App',
     responseType: 'code',
@@ -19,7 +21,8 @@ export const environment = {
   },
   apis: {
     default: {
-      url: 'https://localhost:44349',
+      // url: 'https://localhost:44349',
+      url: 'https://operaapi.gulfbase.com',
       rootNamespace: 'GB.Opera',
     },
   },
